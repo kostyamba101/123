9fb86ff1f7752a4d1c5418fde188e7c0367c481d
kostyamba101 <kostyamba101@gmail.com>
2021-03-28 02:20:22 +0400

    hello world

---
+++ src/main/java/com/project/KonstantinApplication.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonstantinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KonstantinApplication.class, args);
+        System.out.println("Hello World");
+    }
+}
